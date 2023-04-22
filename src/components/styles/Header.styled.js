1d7badf4fa59e0063bd6c837a0eb9144c0dcec4b
Felipe Badaro <felipe_badaro@yahoo.com.br>
2023-04-22 16:41:26 -0300

    doing more header styles

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -2,4 +2,14 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   border: 1px solid green;
+  padding: 10px;
+
+  h1 {
+    color: blue;
+  }
+
+  &:hover {
+    background-color: khaki;
+    cursor: pointer;
+  }
 `;
